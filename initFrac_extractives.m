aafb07b03ffc91095b1da450bb39280f561cea09
boyao <>
2021-04-21 18:19:27 +0200

    0421 school

--- initFrac_extractives.m
+++ initFrac_extractives.m
@@ -6,7 +6,7 @@ global mC mH mO;
 %withAshMoist =[0.4546 0.0621 0.4833];
 %sum(withAshMoist)
 
-CHO = [50.38    6.19    43.43]; %C H O
+CHO = [51 6 43]; %C H O
 CH = [CHO(1)/(CHO(1)+CHO(2)) CHO(2)/(CHO(1)+CHO(2))]
 CHO = CHO./100;
 alpha = 0.68; beta = 0.95; gamma = 0.39;
